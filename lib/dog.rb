e6acb4f0fcf00a113c3ee3971872c25742049a6e
A Glazer <avigayilglazer@gmail.com>
2019-01-30 19:28:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,10 @@
 class Dog
   attr_accessor :name, :breed
 
-  def initialize(params = {})
-    @name = params.fetch(:name, "name")
-    @breed = params.fetch(:breed, "Mutt")
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
   end
 
-def to_s
-  "#{name} #{type}"
-end
+
 end
