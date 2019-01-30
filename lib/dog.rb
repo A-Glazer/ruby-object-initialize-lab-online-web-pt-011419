386914ff3c75fa5de468887c7ef2a7cbb2e47625
A Glazer <avigayilglazer@gmail.com>
2019-01-30 19:48:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,10 @@
 class Dog
   attr_accessor :name, :breed
 
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
     @name = name
-    if breed == nil
-    @breed = "Mutt"
-    else
     @breed = breed
-  end
+  
   end
 
 
